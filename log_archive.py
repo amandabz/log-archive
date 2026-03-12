08741644d1cdd74428c34dc7b7d62234b7930564
amandabz <amandabenitezhidalgo@gmail.com>
2026-03-12 13:55:14 +0100

    feat: add initial log_archive script

---
+++ log_archive.py
@@ -0,0 +1,7 @@
+"""
+this script will be used to archive logs on a set schedule by compressing them
+and storing them in a new directory
+"""
+
+# command to use it (provide the log directory as an argument when running the tool):
+# log-archive <log-directory>

c30c619cbe3f2bc5f754e0c0d6373344e51e97e0
amandabz <amandabenitezhidalgo@gmail.com>
2026-03-12 17:08:12 +0100

    add log compression logic to log_archive.py

--- log_archive.py
+++ log_archive.py
@@ -4,4 +4,28 @@ and storing them in a new directory
 """
 
 # command to use it (provide the log directory as an argument when running the tool):
-# log-archive <log-directory>
+# log-archive <log-directory> >>> logs_archive_20240816_100648.tar.gz
+
+from datetime import datetime
+import tarfile
+import os
+
+
+now = datetime.now()
+format_date = now.strftime("%Y%m%d_%H%M%S")
+print(format_date)
+
+files = ["logs"]
+existing_files = [f for f in files if os.path.exists(f)]
+
+name_tar = f'logs_archive_{format_date}.tar.gz'
+
+# 'w:gz' opens the file for writing with gzip compression
+if existing_files:
+    name_tar = f'logs_archive_{format_date}.tar.gz'
+    with tarfile.open(name_tar, "w:gz") as tar:
+        for file in existing_files:
+            tar.add(file, arcname=os.path.basename(file))
+    print(f"{name_tar} was successfully created.")
+else:
+    print("No logs folder found. No archive was created.")
